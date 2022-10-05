fb9c0d718f4cea492256d824a7d5f9890d688b5b
Chifeng Wen <daviesx66@gmail.com>
2022-10-05 10:28:47 -0700

    Implements the user ingestion service.

---
+++ src/ingestion/services/user_profile_service.py
@@ -0,0 +1,46 @@
+import grpc
+from typing import Any
+
+from src.ingestion.database.common import UserProfileEntity
+from src.ingestion.database.writer import WriteUserProfiles
+from src.ingestion.proto_py.user_ingestion_service_pb2 import WriteProfilesRequest
+from src.ingestion.proto_py.user_ingestion_service_pb2 import WriteProfilesResponse
+from src.ingestion.proto_py.user_ingestion_service_pb2_grpc import UserIngestionServicer
+
+
+class UserIngestionService(UserIngestionServicer):
+    """A service to support the recording of user metadata to the database.
+    """
+
+    def __init__(self, pg_conn: Any) -> None:
+        """Constructs a user ingestion service.
+
+        Args:
+            pg_conn (psycopg2.connection): A psycopg2 connection which connects
+                to the ingestion database.
+        """
+        super().__init__()
+        self.pg_conn = pg_conn
+
+    def WriteProfiles(self,
+                      request: WriteProfilesRequest,
+                      context: grpc.ServicerContext) -> WriteProfilesResponse:
+        """It creates/updates a list of new user profiles, keyed by the user
+        IDs. The call overwrites the profiles if the they have already been
+        created in the system.
+
+        Args:
+            request (WriteProfilesRequest): See user_ingestion_service.proto.
+
+        Returns:
+            WriteProfilesResponse: See user_ingestion_service.proto.
+        """
+        to_be_written = list()
+        for profile in request.user_profiles:
+            entity = UserProfileEntity(user_id=profile.user_id)
+            to_be_written.append(entity)
+
+        WriteUserProfiles(user_profiles=to_be_written, conn=self.pg_conn)
+
+        context.set_code(grpc.StatusCode.OK)
+        return WriteProfilesResponse()
