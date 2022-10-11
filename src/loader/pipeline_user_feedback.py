48f783f57825b09790409f6862bdfffcfd497652
Chifeng Wen <daviesx66@gmail.com>
2022-10-11 14:20:26 -0700

    Updates the feedback loader pipeline to also load user tagging feedbacks.

--- src/loader/pipeline_user_feedback.py
+++ src/loader/pipeline_user_feedback.py
@@ -2,13 +2,14 @@ from typing import Tuple
 
 from src.loader.reader_movie_lens import *
 from src.loader.uploader import UploadDataFrame
-from src.loader.uploader_user_feedback import UserFeedbackUploader
+from src.loader.uploader_user_feedback import UserRatingUploader
+from src.loader.uploader_user_feedback import UserTaggingUploader
 
 
 def LoadMovieLensUserFeedbacks(data_set: MovieLensDataset,
                                feedback_host: str) -> Tuple[int, int]:
-    """Gathers user-content rating feedback data and uploads them to the
-    feedback server.
+    """Gathers user-content rating and tagging feedback data and uploads them
+    to the feedback server.
 
     Args:
         data_set (MovieLensDataset): The Movie Lens data set.
@@ -16,17 +17,29 @@ def LoadMovieLensUserFeedbacks(data_set: MovieLensDataset,
             server.
 
     Returns:
-        Tuple[int, int]: #rating feedbacks and #failed feedbacks.
+        Tuple[int, int]: #total feedbacks and #failed feedbacks.
     """
     data_set.df_ratings.cache()
 
-    uploader = UserFeedbackUploader(host=feedback_host,
-                                    col_name_user_id=RATINGS_COL_USER_ID,
-                                    col_name_content_id=RATINGS_COL_MOVIE_ID,
-                                    col_name_timestamp=RATINGS_COL_TIMESTAMP,
-                                    col_name_rating=RATINGS_COL_RATING)
-    failed_records = UploadDataFrame(data_frame=data_set.df_ratings,
-                                     uploader=uploader,
-                                     num_retries=4)
+    tagging_uploader = UserTaggingUploader(
+        host=feedback_host,
+        col_name_user_id=TAGS_COL_USER_ID,
+        col_name_content_id=TAGS_COL_MOVIE_ID,
+        col_name_timestamp=TAGS_COL_TIMESTAMP,
+        col_name_tag=TAGS_COL_TAG)
+    tagging_failures = UploadDataFrame(data_frame=data_set.df_tags,
+                                       uploader=tagging_uploader,
+                                       num_retries=4)
 
-    return data_set.df_ratings.count(), failed_records.count()
+    rating_uploader = UserRatingUploader(
+        host=feedback_host,
+        col_name_user_id=RATINGS_COL_USER_ID,
+        col_name_content_id=RATINGS_COL_MOVIE_ID,
+        col_name_timestamp=RATINGS_COL_TIMESTAMP,
+        col_name_rating=RATINGS_COL_RATING)
+    rating_failures = UploadDataFrame(data_frame=data_set.df_ratings,
+                                      uploader=rating_uploader,
+                                      num_retries=4)
+
+    return data_set.df_tags.count() + data_set.df_ratings.count(),      \
+        tagging_failures.count() + rating_failures.count()
--- src/loader/uploader_user_feedback.py
+++ src/loader/uploader_user_feedback.py
@@ -3,14 +3,16 @@ from typing import List
 from pyspark import Row
 
 from src.ingestion.proto_py.user_feedback_ingestion_service_pb2 import RatingFeedback
+from src.ingestion.proto_py.user_feedback_ingestion_service_pb2 import TaggingFeedback
 from src.ingestion.proto_py.user_feedback_ingestion_service_pb2 import RecordRatingFeedbacksRequest
+from src.ingestion.proto_py.user_feedback_ingestion_service_pb2 import RecordTaggingFeedbacksRequest
 from src.ingestion.proto_py.user_feedback_ingestion_service_pb2_grpc import UserFeedbackIngestionStub
 from src.loader.uploader import RecordUploaderConnection
 from src.loader.uploader import RecordUploaderInterface
 
 
-class UserFeedbackUploader(RecordUploaderInterface):
-    """Uploads user feedbacks to the user feedback ingestion service.
+class UserRatingUploader(RecordUploaderInterface):
+    """Uploads user rating feedbacks to the user feedback ingestion service.
     """
 
     def __init__(self,
@@ -19,7 +21,7 @@ class UserFeedbackUploader(RecordUploaderInterface):
                  col_name_content_id: str,
                  col_name_timestamp: str,
                  col_name_rating: str) -> None:
-        """Constructs a user feedback uploader.
+        """Constructs a user rating feedback uploader.
 
         Args:
             host (str): The IP address and port which points to the content
@@ -48,21 +50,80 @@ class UserFeedbackUploader(RecordUploaderInterface):
     def UploadBatch(
             self, batch: List[Row],
             connection: RecordUploaderConnection) -> bool:
-        """Uploads user feedbacks in batch.
+        """Uploads user rating feedbacks in batch.
         """
+        request = RecordRatingFeedbacksRequest()
+        for row in batch:
+            rating_feedback = RatingFeedback(
+                user_id=row[self.col_name_user_id],
+                content_id=row[self.col_name_content_id],
+                timestamp_secs=row[self.col_name_timestamp],
+                rating=row[self.col_name_rating])
+
+            request.rating_feedbacks.append(rating_feedback)
         try:
-            request = RecordRatingFeedbacksRequest()
-            for row in batch:
-                profile = RatingFeedback(
-                    user_id=row[self.col_name_user_id],
-                    content_id=row[self.col_name_content_id],
-                    timestamp_secs=row[self.col_name_timestamp],
-                    rating=row[self.col_name_rating])
-
-                request.rating_feedbacks.append(profile)
-
             user_feedback_ingestion_stub: UserFeedbackIngestionStub = connection.stub
             user_feedback_ingestion_stub.RecordRatingFeedbacks(request)
+
+            return True
+        except grpc.RpcError as e:
+            return False
+
+
+class UserTaggingUploader(RecordUploaderInterface):
+    """Uploads user tagging feedbacks to the user feedback ingestion service.
+    """
+
+    def __init__(self,
+                 host: str,
+                 col_name_user_id: str,
+                 col_name_content_id: str,
+                 col_name_timestamp: str,
+                 col_name_tag: str) -> None:
+        """Constructs a user tagging feedback uploader.
+
+        Args:
+            host (str): The IP address and port which points to the content
+                profile ingestion service.
+            col_name_content_id (str): The name of the content ID column in the
+                data frame that needs to be uploaded.
+        """
+        super().__init__()
+
+        self.host = host
+        self.col_name_user_id = col_name_user_id
+        self.col_name_content_id = col_name_content_id
+        self.col_name_timestamp = col_name_timestamp
+        self.col_name_tag = col_name_tag
+
+    def MaxBatchSize(self) -> int:
+        return 200
+
+    def Connect(self) -> RecordUploaderConnection:
+        """Connects to the user feedback ingestion service.
+        """
+        grpc_channel = grpc.insecure_channel(target=self.host)
+        return RecordUploaderConnection(
+            channel=grpc_channel, stub=UserFeedbackIngestionStub(grpc_channel))
+
+    def UploadBatch(
+            self, batch: List[Row],
+            connection: RecordUploaderConnection) -> bool:
+        """Uploads user tagging feedbacks in batch.
+        """
+        request = RecordTaggingFeedbacksRequest()
+        for row in batch:
+            tagging_feedback = TaggingFeedback(
+                user_id=row[self.col_name_user_id],
+                content_id=row[self.col_name_content_id],
+                tag=row[self.col_name_tag],
+                timestamp_secs=row[self.col_name_timestamp])
+
+            request.tagging_feedbacks.append(tagging_feedback)
+
+        try:
+            user_feedback_ingestion_stub: UserFeedbackIngestionStub = connection.stub
+            user_feedback_ingestion_stub.RecordTaggingFeedbacks(request)
 
             return True
         except grpc.RpcError as e:
