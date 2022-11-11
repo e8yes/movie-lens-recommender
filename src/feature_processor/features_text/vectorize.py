180182c4224d8b16f5adfcfdb17f7a678930f2ac
Chifeng Wen <daviesx66@gmail.com>
2022-11-10 19:19:32 -0800

    Adds a function which helps load in GloVe word embedding definitions.

--- src/feature_processor/features_text/vectorize.py
+++ src/feature_processor/features_text/vectorize.py
@@ -1,17 +1,58 @@
-from pyspark.sql import DataFrame
+import os
+from pyspark.sql import DataFrame, Row, SparkSession, types
 
 
-def LoadGloveDefinitions(path: str, dimension: int) -> DataFrame:
-    """_summary_
+def _ParseLine(line: str) -> Row:
+    parts = line.split(sep=" ")
+
+    word = parts[0]
+
+    embedding = list()
+    for i in range(1, len(parts)):
+        embedding.append(float(parts[i]))
+
+    return Row(word=word, embedding=embedding)
+
+
+def LoadGloveDefinitions(path: str,
+                         dimension: int,
+                         spark: SparkSession) -> DataFrame:
+    """Loads GloVe word embeddings definitions as a dataframe.
+
+    Example output:
+    --------------------------------------------
+    | word  | embedding                        |
+    --------------------------------------------
+    | "the" | [0.418, 0.24968, -0.41242, ...]  |
+    --------------------------------------------
+    | "won" | [-1.5561, 0.86241, 0.14604, ...] |
+    --------------------------------------------
 
     Args:
-        path (str): _description_
-        dimension (int): _description_
+        path (str): Path to which GloVe word embeddings txt files are located.
+        dimension (int): The size of word embeddins to use. Value can be 50,
+            100, 200 and 300.
 
     Returns:
-        DataFrame: _description_
+        DataFrame: See the example output above.
     """
-    pass
+    glove_file_name = "glove.6B.{0}d.txt".format(dimension)
+    glove_path = os.path.join(path, glove_file_name)
+
+    schema = types.StructType(
+        fields=[
+            types.StructField(
+                name="word", dataType=types.StringType()),
+            types.StructField(
+                name="embedding", dataType=types.ArrayType(
+                    elementType=types.FloatType()))
+        ])
+
+    return spark.                   \
+        sparkContext.               \
+        textFile(name=glove_path).  \
+        map(_ParseLine).            \
+        toDF(schema=schema)
 
 
 def VectorizeContentTokens(content_tokens: DataFrame,
