c7f779c50e646ab86a54d44561fc4c9b0d90ebad
Chifeng Wen <daviesx66@gmail.com>
2022-11-24 14:16:08 -0800

    Adds an ingestion schema for the Cassandra store.

--- src/ingestion/push_schema.sh
+++ src/ingestion/push_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Ensure that the postgres db engine is started.
+# Ensure that the PostgreSQL DB engine is started.
 sudo systemctl start postgresql
 
 set +e # The database might have already existed, but that's fine.
@@ -10,3 +10,8 @@ set -e
 echo "ALTER DATABASE ingestion OWNER TO postgres;" | sudo -u postgres psql ingestion
 echo "ALTER USER postgres WITH PASSWORD '0f21e4cd-44f8-48ab-b112-62030d7f7df1';" | sudo -u postgres psql ingestion
 cat 'schema/ingestion.pgsql' | sudo -u postgres psql ingestion
+
+# Ensure that the Cassandra DB engine is started.
+sudo systemctl start cassandra
+echo "CREATE KEYSPACE IF NOT EXISTS ingestion WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1 };" | cqlsh
+cat 'schema/ingestion.cql' | cqlsh
