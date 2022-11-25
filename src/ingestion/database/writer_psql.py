bf15599fbb581123901aa0c96578cecadd7e83ea
Chifeng Wen <daviesx66@gmail.com>
2022-11-24 19:12:42 -0800

    Fixes the PostgreSQL ingestion writer for the same bug as well.

--- src/ingestion/database/writer_psql.py
+++ src/ingestion/database/writer_psql.py
@@ -118,7 +118,8 @@ class PostgresIngestionWriter(IngestionWriterInterface):
                 content_profile.title,
                 content_profile.genres,
                 json.dumps(content_profile.tags,
-                           cls=StatelessClassJsonEncoder),
+                           cls=StatelessClassJsonEncoder)
+                if content_profile.tags is not None else None,
                 content_profile.imdb_id,
                 content_profile.tmdb_id,
             ))
@@ -227,9 +228,12 @@ class PostgresIngestionWriter(IngestionWriterInterface):
             (
                 content_id,
                 tmdb.tmdb_id,
-                json.dumps(tmdb.primary_info),
-                json.dumps(tmdb.credits),
-                json.dumps(tmdb.keywords),
+                json.dumps(tmdb.primary_info)
+                if tmdb.primary_info is not None else None,
+                json.dumps(tmdb.credits)
+                if tmdb.credits is not None else None,
+                json.dumps(tmdb.keywords)
+                if tmdb.keywords is not None else None,
             )
         ]
 
@@ -259,7 +263,8 @@ class PostgresIngestionWriter(IngestionWriterInterface):
             (
                 content_id,
                 imdb.imdb_id,
-                json.dumps(imdb.primary_info),
+                json.dumps(imdb.primary_info)
+                if imdb.primary_info is not None else None,
             )
         ]
 
