0b8fb7839da55404cd8e0a427440c46f64ee50ee
Fei Min <ffmm6521@gmail.com>
2022-12-08 21:57:41 -0800

    Writer functions and feature generator main.

--- src/feature_processor/main_features_gen.py
+++ src/feature_processor/main_features_gen.py
@@ -1,41 +1,61 @@
 import argparse
 from os import path
+from pyspark.sql import SparkSession
+from typing import List
 
-from src.feature_processor.pipelines.writer import WriteAsTfRecordDataSet
+from src.feature_processor.pipelines.writer import WriteAsParquetDataSet
 from src.feature_processor.pipelines.assemble_features \
     import AssembleContentFeatures
 from src.feature_processor.pipelines.assemble_features \
     import AssembleUserFeatures
-from src.ingestion.database.reader import IngestionReader
+from src.ingestion.database.factory import IngestionReaderFactory
 
 
-def Main(postgres_host: str,
+def Main(cassandra_contact_points: List[str],
+         postgres_host: str,
          postgres_user: str,
          postgres_password: str,
          output_path: str):
-    reader = IngestionReader(db_host=postgres_host,
-                             db_user=postgres_user,
-                             db_password=postgres_password)
+    reader_factory = IngestionReaderFactory(
+        cassandra_contact_points=cassandra_contact_points,
+        postgres_host=postgres_host,
+        postgres_user=postgres_user,
+        postgres_password=postgres_password)
+
+    builder = SparkSession.builder.appName("Feature Generator")
+    builder = reader_factory.ConfigureSparkSession(spark_builder=builder)
+    spark = builder.getOrCreate()
+
+    reader = reader_factory.Create(spark=spark)
 
     content_features = AssembleContentFeatures(reader=reader)
     user_features = AssembleUserFeatures(reader=reader)
 
-    WriteAsTfRecordDataSet(
-        df=content_features, spark=reader.spark_session, output_path=path.join(
-            output_path, "content_features"))
-    WriteAsTfRecordDataSet(df=user_features,
-                           spark=reader.spark_session,
-                           output_path=path.join(output_path, "user_features"))
+    WriteAsParquetDataSet(
+        df=content_features,
+        spark=spark,
+        output_path=path.join(output_path, "content_features"))
+    WriteAsParquetDataSet(
+        df=user_features,
+        spark=reader.spark,
+        output_path=path.join(output_path, "user_features"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Extracts and transforms information drawn from the raw \
-data set to create user and content features for modeling.")
+        description="Extracts and transforms information drawn from the raw "
+        "data set to create user and content features for modeling.")
     parser.add_argument(
-        name_or_flags="--postgres_host", type=str,
-        help="The IP address which points to the postgres database server \
-which stores the raw ingestion data set.")
+        "--cassandra_contact_points",
+        nargs="+",
+        type=str,
+        help="The list of contact points to try connecting for Cassandra "
+             "cluster discovery.")
+    parser.add_argument(
+        name_or_flags="--postgres_host",
+        type=str,
+        help="The IP address which points to the postgres database server "
+        "which stores the raw ingestion data set.")
     parser.add_argument(
         name_or_flags="--postgres_user",
         type=str,
@@ -45,26 +65,20 @@ which stores the raw ingestion data set.")
         type=str,
         help="The password of the postgres user.")
     parser.add_argument(
-        name_or_flags="--output_path", type=str, nargs="+",
-        help="Path where the user and content features are going to be written\
-.")
+        name_or_flags="--output_path",
+        type=str,
+        nargs="+",
+        help="Path where the user and content features are going to be written"
+        ".")
 
     args = parser.parse_args()
 
-    if args.postgres_host is None:
-        print("postgres_host is required.")
-        exit(-1)
-    if args.postgres_user is None:
-        print("postgres_user is required.")
-        exit(-1)
-    if args.postgres_password is None:
-        print("postgres_password is required.")
-        exit(-1)
     if args.output_path is None:
         print("output_path is required.")
         exit(-1)
 
-    Main(postgres_host=args.postgres_host,
+    Main(cassandra_contact_points=args.cassandra_contact_points,
+         postgres_host=args.postgres_host,
          postgres_user=args.postgres_user,
          postgres_password=args.postgres_password,
          output_path=args.output_path)
--- src/feature_processor/pipelines/writer.py
+++ src/feature_processor/pipelines/writer.py
@@ -4,4 +4,16 @@ from pyspark.sql import DataFrame, SparkSession
 def WriteAsTfRecordDataSet(df: DataFrame,
                            spark: SparkSession,
                            output_path: str) -> None:
-    pass
+    try:
+        df.write.\
+            format("tfrecords").\
+            option("recordType", "Example").\
+            option("codec", "org.apache.hadoop.io.compress.GzipCodec").\
+            mode("overwrite").\
+            save(path=output_path)
+    except Exception:
+        pass
+
+
+def WriteAsParquetDataSet(df: DataFrame, output_path: str) -> None:
+    df.write.parquet(path=output_path, mode="overwrite")
