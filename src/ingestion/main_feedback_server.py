4f5e4927e5fd1d20685e9e10f3c27177ecec6312
Chifeng Wen <daviesx66@gmail.com>
2022-12-08 19:23:14 -0800

    Uses the ingestion writer factory abstraction so it can write to either postgresql or cassandra based on command line inputs.

--- src/ingestion/main_feedback_server.py
+++ src/ingestion/main_feedback_server.py
@@ -2,8 +2,7 @@ import argparse
 import grpc
 from concurrent import futures
 
-from src.ingestion.database.writer import IngestionWriterInterface
-from src.ingestion.database.writer_psql import PostgresIngestionWriter
+from src.ingestion.database.factory import IngestionWriterFactory
 from src.ingestion.proto_py.user_feedback_ingestion_service_pb2_grpc import \
     add_UserFeedbackIngestionServicer_to_server
 from src.ingestion.services.user_feedback_service import \
@@ -11,8 +10,10 @@ from src.ingestion.services.user_feedback_service import \
 
 
 def __RunServer(
-        grpc_port: int, ingestion_writer: IngestionWriterInterface) -> None:
+        grpc_port: int, writer_factory: IngestionWriterFactory) -> None:
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    ingestion_writer = writer_factory.Create()
 
     user_feedback_ingestion_service = UserFeedbackIngestionService(
         writer=ingestion_writer)
@@ -31,30 +32,34 @@ def __RunServer(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Launches a gRPC server which serves the user feedback \
-ingestion services.")
+        description="Launches a gRPC server which serves the user feedback "
+                    "ingestion services.")
     parser.add_argument("--grpc_port",
                         type=int,
                         help="The port number on which the gRPC server runs.")
     parser.add_argument(
-        "--postgres_host", type=str,
+        "--cassandra_contact_points",
+        nargs="+",
+        type=str,
+        help="The list of contact points to try connecting for Cassandra "
+             "cluster discovery.")
+    parser.add_argument(
+        "--postgres_host",
+        type=str,
         help="The IP address which points to the postgres database server.")
-    parser.add_argument("--postgres_password",
-                        type=str,
-                        help="The password of the postgres database user.")
+    parser.add_argument(
+        "--postgres_password",
+        type=str,
+        help="The password of the postgres database user.")
 
     args = parser.parse_args()
 
     if args.grpc_port is None:
         print("grpc_port is required.")
         exit(-1)
-    if args.postgres_host is None:
-        print("postgres_host is required.")
-        exit(-1)
-    if args.postgres_password is None:
-        print("postgres_password is required.")
-        exit(-1)
 
-    ingestion_writer = PostgresIngestionWriter(host=args.postgres_host,
-                                               password=args.postgres_password)
-    __RunServer(grpc_port=args.grpc_port, ingestion_writer=ingestion_writer)
+    writer_factory = IngestionWriterFactory(
+        cassandra_contact_points=args.cassandra_contact_points,
+        postgres_host=args.postgres_host,
+        postgres_password=args.postgres_password)
+    __RunServer(grpc_port=args.grpc_port, writer_factory=writer_factory)
--- src/ingestion/main_ingestion_server.py
+++ src/ingestion/main_ingestion_server.py
@@ -2,8 +2,7 @@ import argparse
 import grpc
 from concurrent import futures
 
-from src.ingestion.database.writer import IngestionWriterInterface
-from src.ingestion.database.writer_psql import PostgresIngestionWriter
+from src.ingestion.database.factory import IngestionWriterFactory
 from src.ingestion.proto_py.content_ingestion_service_pb2_grpc import \
     add_ContentIngestionServicer_to_server
 from src.ingestion.proto_py.user_ingestion_service_pb2_grpc import \
@@ -14,13 +13,16 @@ from src.ingestion.services.user_profile_service import UserIngestionService
 
 
 def __RunServer(grpc_port: int,
-                ingestion_writer: IngestionWriterInterface,
+                writer_factory: IngestionWriterFactory,
                 kafka_host: str):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    ingestion_writer = writer_factory.Create()
 
     user_ingestion_service = UserIngestionService(writer=ingestion_writer)
     content_ingestion_service = ContentIngestionService(
         writer=ingestion_writer, kafka_host=kafka_host)
+
     add_UserIngestionServicer_to_server(
         servicer=user_ingestion_service, server=server)
     add_ContentIngestionServicer_to_server(
@@ -38,39 +40,43 @@ def __RunServer(grpc_port: int,
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Launches a gRPC server which serves the ingestion \
-services.")
+        description="Launches a gRPC server which serves the ingestion "
+                    "services.")
     parser.add_argument("--grpc_port",
                         type=int,
                         help="The port number on which the gRPC server runs.")
     parser.add_argument(
+        "--cassandra_contact_points",
+        nargs="+",
+        type=str,
+        help="The list of contact points to try connecting for Cassandra "
+             "cluster discovery.")
+    parser.add_argument(
         "--postgres_host", type=str,
         help="The IP address which points to the postgres database server.")
-    parser.add_argument("--postgres_password",
-                        type=str,
-                        help="The password of the postgres database user.")
+    parser.add_argument(
+        "--postgres_password",
+        type=str,
+        help="The password of the postgres database user.")
     parser.add_argument(
         "--kafka_host", type=str,
-        help="The host address (with port number) which points to the Kafka \
-server.")
+        help="The host address (with port number) which points to the Kafka "
+             "server.")
 
     args = parser.parse_args()
 
     if args.grpc_port is None:
         print("grpc_port is required.")
         exit(-1)
-    if args.postgres_host is None:
-        print("postgres_host is required.")
-        exit(-1)
-    if args.postgres_password is None:
-        print("postgres_password is required.")
-        exit(-1)
     if args.kafka_host is None:
         print("kafka_host is required.")
         exit(-1)
 
-    ingestion_writer = PostgresIngestionWriter(host=args.postgres_host,
-                                               password=args.postgres_password)
+    writer_factory = IngestionWriterFactory(
+        cassandra_contact_points=args.cassandra_contact_points,
+        postgres_host=args.postgres_host,
+        postgres_password=args.postgres_password)
+
     __RunServer(grpc_port=args.grpc_port,
-                ingestion_writer=ingestion_writer,
+                writer_factory=writer_factory,
                 kafka_host=args.kafka_host)
